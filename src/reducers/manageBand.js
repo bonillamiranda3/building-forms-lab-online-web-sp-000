77ffd6bfaab8d3c94d400685809f67e0b699b99b
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-11 22:20:43 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,4 +1,4 @@
-export default function manageBand(state = {bands [],}, action) {
+export default function manageBand(state = {bands: [],}, action) {
   switch (action.type) {
       case 'ADD_BAND':
       return { bands: state.bands.concat({name: action.payload.name})};
